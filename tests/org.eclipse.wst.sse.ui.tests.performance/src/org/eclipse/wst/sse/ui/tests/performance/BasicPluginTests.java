91e395c22785bf56ece98500e5c1db38fbbc0563
david_williams <david_williams>
2005-03-29 21:07:29 +0000

    removed old deprecated editor plugin

--- tests/org.eclipse.wst.sse.ui.tests.performance/src/org/eclipse/wst/sse/ui/tests/performance/BasicPluginTests.java
+++ tests/org.eclipse.wst.sse.ui.tests.performance/src/org/eclipse/wst/sse/ui/tests/performance/BasicPluginTests.java
@@ -1,18 +1,18 @@
 package org.eclipse.wst.sse.ui.tests.performance;
 
 import org.eclipse.test.performance.PerformanceTestCase;
-import org.eclipse.wst.sse.ui.EditorPlugin;
+import org.eclipse.wst.sse.ui.internal.SSEUIPlugin;
 
 /**
  * @author pavery
  */
 public class BasicPluginTests extends PerformanceTestCase {
-	
+
 	public void testInitializePlugin() {
 		// just a dummy first test
 		startMeasuring();
 		stopMeasuring();
-		EditorPlugin.getDefault();
+		SSEUIPlugin.getDefault();
 		commitMeasurements();
 		assertPerformance();
 	}
