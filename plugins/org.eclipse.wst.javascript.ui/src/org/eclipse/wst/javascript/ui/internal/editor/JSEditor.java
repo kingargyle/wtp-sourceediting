aecb8d8aeab344d82edfdd5ac169ab187f93be6b
nitind <nitind>
2005-11-09 21:23:25 +0000

    document use of "debug" ruler context menu group

--- plugins/org.eclipse.wst.javascript.ui/src/org/eclipse/wst/javascript/ui/internal/editor/JSEditor.java
+++ plugins/org.eclipse.wst.javascript.ui/src/org/eclipse/wst/javascript/ui/internal/editor/JSEditor.java
@@ -980,10 +980,11 @@ public class JSEditor extends TextEditor {
 	}
 
 	protected void rulerContextMenuAboutToShow(IMenuManager menu) {
+		super.rulerContextMenuAboutToShow(menu);
+		// append actions to "debug" group (created in AbstractDecoratedTextEditor.rulerContextMenuAboutToShow(IMenuManager))
 		menu.appendToGroup("debug", getAction(ActionDefinitionIds.TOGGLE_BREAKPOINTS));
 		menu.appendToGroup("debug", getAction(ActionDefinitionIds.MANAGE_BREAKPOINTS));
 		menu.appendToGroup("debug", getAction(ActionDefinitionIds.EDIT_BREAKPOINTS));
-		super.rulerContextMenuAboutToShow(menu);
 	}
 
 	/*
