5944ac2e546160f8cc2097610637626c30a8b788
david_williams <david_williams>
2006-02-14 01:30:38 +0000

    undated old example due to partition changes

--- examples/org.eclipse.wst.xml.examples.cheat-sheet-editor/src/org/eclipse/wst/xml/examples/cs/ui/StructuredTextViewerConfigurationCS.java
+++ examples/org.eclipse.wst.xml.examples.cheat-sheet-editor/src/org/eclipse/wst/xml/examples/cs/ui/StructuredTextViewerConfigurationCS.java
@@ -13,8 +13,8 @@ package org.eclipse.wst.xml.examples.cs.ui;
 
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredPartitionTypes;
-import org.eclipse.wst.xml.core.internal.provisional.text.IXMLPartitions;
+import org.eclipse.wst.sse.core.text.IStructuredPartitions;
+import org.eclipse.wst.xml.core.text.IXMLPartitions;
 import org.eclipse.wst.xml.examples.cs.ui.contentassist.CSContentAssistProcessor;
 import org.eclipse.wst.xml.ui.StructuredTextViewerConfigurationXML;
 import org.eclipse.wst.xml.ui.internal.contentassist.NoRegionContentAssistProcessor;
@@ -25,11 +25,12 @@ public class StructuredTextViewerConfigurationCS extends StructuredTextViewerCon
 	protected IContentAssistProcessor[] getContentAssistProcessors(ISourceViewer sourceViewer, String partitionType) {
 		IContentAssistProcessor[] processors = null;
 
-		if ((partitionType == IStructuredPartitionTypes.DEFAULT_PARTITION) || (partitionType == IXMLPartitions.XML_DEFAULT)) {
+		if ((partitionType == IStructuredPartitions.DEFAULT_PARTITION) || (partitionType == IXMLPartitions.XML_DEFAULT)) {
 			processors = new IContentAssistProcessor[]{new CSContentAssistProcessor()};
 		}
-		// ISSUE: this should be unneeded, right? Just supplied by super class? 
-		else if (partitionType == IStructuredPartitionTypes.UNKNOWN_PARTITION) {
+		// ISSUE: this should be unneeded, right? Just supplied by super
+		// class?
+		else if (partitionType == IStructuredPartitions.UNKNOWN_PARTITION) {
 			processors = new IContentAssistProcessor[]{new NoRegionContentAssistProcessor()};
 		}
 
