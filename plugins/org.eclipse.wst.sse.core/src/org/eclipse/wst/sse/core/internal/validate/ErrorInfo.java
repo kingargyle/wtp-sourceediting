cd3d4c023350c70360ff04ebe47ecd132426e3e1
nitind <nitind>
2008-03-25 18:12:13 +0000

    [221606] Make (HTML) validation message severity user controllable

--- plugins/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/validate/ErrorInfo.java
+++ plugins/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/validate/ErrorInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,22 +7,20 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Jens Lukowski/Innoopract - initial renaming/restructuring
- *     
  *******************************************************************************/
 package org.eclipse.wst.sse.core.internal.validate;
 
 
 
+public interface ErrorInfo {
 
-/**
- */
-public interface ValidationReporter {
+	public String getHint();
 
-	/**
-	 */
-	void report(ValidationMessage message);
-	
-	void report(ErrorInfo info);
-	
+	public int getLength();
+
+	public int getOffset();
+
+	public int getState();
+
+	public short getTargetType();
 }
