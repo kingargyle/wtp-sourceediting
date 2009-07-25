21cfeeb2c0d923a9624a833286cf23095cf08195
david_williams <david_williams>
2009-07-25 01:21:16 +0000

    save jit test

--- development/org.eclipse.wst.sse.tokenizerJitTest/runtest.sh
+++ development/org.eclipse.wst.sse.tokenizerJitTest/runtest.sh
@@ -1,6 +1,11 @@
-Sam
+# Sample script to invoke the test code
 
 export JAVA_HOME=/shared/webtools/apps/ibm-java-ppc-605
+
+# if no options below are uncommented, the test jar should exhibit the 
+# error condition after a few thousand files. 
+# You can use almost any directory, as long as there are thousands of files, 
+# that can be accessed for read. 
 
 # reduce optimization level 
 #export IBM_JAVA_OPTIONS=-Xjit:"{org/eclipse/wst/sse/tokenizerJitTest/IntStack;)V|org/eclipse/wst/sse/tokenizerJitTest/HTMLHeadTokenizer.*}(optLevel=scorching)"
