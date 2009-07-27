ecc7a078b49a651e1a6ec5a53f880a8828d935fa
david_williams <david_williams>
2009-07-27 21:02:55 +0000

    save jit test

---
+++ development/org.eclipse.wst.sse.tokenizerJitTestApps/src/org/eclipse/wst/sse/tokenizerjittestApps/JSPedCSSTokenizerTesterForDirectory.java
@@ -0,0 +1,55 @@
+package org.eclipse.wst.sse.tokenizerjittestApps;
+
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.eclipse.jst.jsp.css.core.internal.parser.JSPedCSSTokenizer;
+import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
+
+/**
+ * This class was based on a JUnit test, HTMLHeadTokenizerTester, and then
+ * simplified to run as a standalone Eclipse app, to test for JIT errors.
+ * 
+ * @author davidw
+ * 
+ */
+public class JSPedCSSTokenizerTesterForDirectory extends TokenizerTesterForDirectory {
+
+	static final String tokenizerName = "JSPedCSSTokenizer";
+
+	@Override
+	protected void doTest(final Reader reader) throws IOException {
+
+		final JSPedCSSTokenizer tokenizer = new JSPedCSSTokenizer(reader);
+		scanThroughFile(tokenizer);
+	}
+
+	@Override
+	protected String getTokenizerName() {
+		return tokenizerName;
+	}
+
+	private void scanThroughFile(final JSPedCSSTokenizer tokenizer) throws IOException {
+		ITextRegion token = null;
+		do {
+			token = tokenizer.getNextToken();
+			if (DEBUG) {
+				System.out.println(token);
+			}
+		}
+		while (!tokenizer.isEOF());
+
+	}
+}
