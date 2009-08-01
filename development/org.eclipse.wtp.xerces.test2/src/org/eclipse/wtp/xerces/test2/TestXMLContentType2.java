94b95955bf178debfe827e7814ea7bfc762b9e06
david_williams <david_williams>
2009-08-01 04:01:40 +0000

    xerces tests

---
+++ development/org.eclipse.wtp.xerces.test2/src/org/eclipse/wtp/xerces/test2/TestXMLContentType2.java
@@ -0,0 +1,161 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wtp.xerces.test2;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.xml.sax.SAXException;
+
+public class TestXMLContentType2 implements IApplication { // extends TestCase
+
+	private static final int OSGI_SERVICE = 11;
+	private static final int DIRECT_INSTANTIATION = 12;
+	private static final int SERVICE_WITH_CONTEXT_CLASSLOADER = 13;
+	private static final boolean DO_FAILING_ORDER = false;
+	private Boolean applicationResult;
+
+
+	public TestXMLContentType2() {
+		super();
+	}
+
+	public Object start(IApplicationContext context) throws Exception {
+		applicationResult = null;
+		doTests();
+		return applicationResult;
+	}
+
+	private void handleTestResult(boolean testOk, String testName) {
+		/*
+		 * always set application result if its null, but do not change to
+		 * true, if it is already false.
+		 */
+		if (applicationResult == null) {
+			applicationResult = new Boolean(testOk);
+		}
+		else if (!testOk) {
+			applicationResult = new Boolean(false);
+		}
+
+		if (testOk) {
+			System.out.println("\n     Test OK: " + testName + "\n");
+		}
+		else {
+			System.out.println("\n     Test Failed: " + testName + "\n");
+		}
+	}
+
+	public void stop() {
+		// no need to do any thing
+	}
+
+	public boolean testGetParser(int factoryMethod) throws ParserConfigurationException, SAXException {
+		boolean result = false;
+
+		SAXParser parser = null;
+		SAXParserFactory factory = null;
+
+		switch (factoryMethod) {
+			case OSGI_SERVICE :
+				factory = WTPTestXercesPlugin2.instance().getFactoryWithOSGiService();
+				break;
+			case DIRECT_INSTANTIATION :
+				factory = WTPTestXercesPlugin2.instance().getFactoryWithDirectInstantiation();
+				break;
+			case SERVICE_WITH_CONTEXT_CLASSLOADER :
+				factory = WTPTestXercesPlugin2.instance().getFactoryWithThreadContextClassloader();
+				break;
+			default :
+				throw new IllegalArgumentException("Program Error: invalid value provided for Factory Creation Method");
+		}
+
+
+		if (factory == null) {
+			result = false;
+		}
+		else {
+
+			System.out.println("SAXParserFactory class: " + factory.getClass());
+			System.out.println("SAXParserFactory classloader: " + factory.getClass().getClassLoader());
+
+			try {
+				parser = factory.newSAXParser();
+				// if we get a parser, just say true for this test
+				result = true;
+			}
+			catch (final ClassCastException e) {
+				// catch and print class cast for test
+				System.out.println(e);
+				result = false;
+			}
+
+		}
+
+
+		return result;
+	}
+
+	public void doTests() throws ParserConfigurationException, SAXException {
+		boolean testOk = false;
+		String testName = null;
+
+		testName = "DIRECT_INSTANTIATION";
+		System.out.println("\n     Do test: " + testName + "\n");
+		testOk = testGetParser(DIRECT_INSTANTIATION);
+		handleTestResult(testOk, testName);
+
+		/*
+		 * Note: SERVICE_WITH_CONTEXT_CLASSLOADER and OSGI_SERVICE are order
+		 * sensitive.
+		 * 
+		 * Both tests fail if OSGI_SERVICE method used first (registers a
+		 * "base class loader" version of factory for all time.
+		 * 
+		 * Both tests pass if SERVICE_WITH_CONTEXT_CLASSLOADER method used
+		 * first (registers a xerces bundle version of factory).
+		 * 
+		 * The DIRECT_INSTANTIATION method seems to always work.
+		 */
+		if (DO_FAILING_ORDER) {
+			System.out.println("\n  Doing failing order tests. \n");
+			testName = "OSGI_SERVICE";
+			System.out.println("\n     Do test: " + testName + "\n");
+			testOk = testGetParser(OSGI_SERVICE);
+			handleTestResult(testOk, testName);
+
+			testName = "SERVICE_WITH_CONTEXT_CLASSLOADER";
+			System.out.println("\n       Do test: " + testName + "\n");
+			testOk = testGetParser(SERVICE_WITH_CONTEXT_CLASSLOADER);
+			handleTestResult(testOk, testName);
+		}
+		else {
+			System.out.println("\n Doing successful order tests. \n");
+			testName = "SERVICE_WITH_CONTEXT_CLASSLOADER";
+			System.out.println("\n       Do test: " + testName + "\n");
+			testOk = testGetParser(SERVICE_WITH_CONTEXT_CLASSLOADER);
+			handleTestResult(testOk, testName);
+
+			testName = "OSGI_SERVICE";
+			System.out.println("\n     Do test: " + testName + "\n");
+			testOk = testGetParser(OSGI_SERVICE);
+			handleTestResult(testOk, testName);
+		}
+		testName = "DIRECT_INSTANTIATION";
+		System.out.println("\n     Do test: " + testName + "\n");
+		testOk = testGetParser(DIRECT_INSTANTIATION);
+		handleTestResult(testOk, testName);
+	}
+
+}
---
+++ development/org.eclipse.wtp.xerces.test2/src/org/eclipse/wtp/xerces/test2/WTPTestXercesPlugin2.java
@@ -0,0 +1,126 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Oracle - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.wtp.xerces.test2;
+
+import javax.xml.parsers.SAXParserFactory;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * Test Plugin, originally from JPT.
+ * 
+ */
+
+public class WTPTestXercesPlugin2 extends Plugin {
+
+
+
+	private ServiceTracker parserTracker;
+
+	// ********** singleton **********
+
+	private static WTPTestXercesPlugin2 INSTANCE;
+
+	/**
+	 * Return the singleton JPT plug-in.
+	 */
+	public static WTPTestXercesPlugin2 instance() {
+		return INSTANCE;
+	}
+
+
+	// ********** public static methods **********
+
+
+	/**
+	 * Log the specified status.
+	 */
+	public static void log(IStatus status) {
+		INSTANCE.getLog().log(status);
+	}
+
+
+	// ********** plug-in implementation **********
+
+	public WTPTestXercesPlugin2() {
+		super();
+	}
+
+
+	public SAXParserFactory getFactoryWithDirectInstantiation() {
+		SAXParserFactory theFactory = null;
+
+		theFactory = SAXParserFactory.newInstance();
+
+		return theFactory;
+	}
+
+	public SAXParserFactory getFactoryWithOSGiService() {
+		SAXParserFactory theFactory = null;
+		if (parserTracker == null) {
+			parserTracker = new ServiceTracker(getBundle().getBundleContext(), "javax.xml.parsers.SAXParserFactory", null);
+
+			parserTracker.open(true);
+		}
+		theFactory = (SAXParserFactory) parserTracker.getService();
+		return theFactory;
+	}
+
+	public SAXParserFactory getFactoryWithThreadContextClassloader() {
+		SAXParserFactory theFactory = null;
+		/*
+		 * set context class loader while factories instantiate classes (for
+		 * minimal time) so proper classes, and classloaders, are used in IBM
+		 * JRE, with Xerces on the classpath. See bug 283721.
+		 * (https://bugs.eclipse.org/bugs/show_bug.cgi?id=283721)
+		 */
+
+		final ClassLoader savedClassLoader = Thread.currentThread().getContextClassLoader();
+		try {
+			Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+			if (parserTracker == null) {
+				parserTracker = new ServiceTracker(getBundle().getBundleContext(), "javax.xml.parsers.SAXParserFactory", null);
+
+				parserTracker.open(true);
+			}
+			theFactory = (SAXParserFactory) parserTracker.getService();
+		}
+		finally {
+			Thread.currentThread().setContextClassLoader(savedClassLoader);
+		}
+
+
+		return theFactory;
+	}
+
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		INSTANCE = this;
+	}
+
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		try {
+			if (parserTracker != null) {
+				parserTracker.close();
+				parserTracker = null;
+			}
+		}
+		finally {
+			super.stop(context);
+		}
+	}
+}
