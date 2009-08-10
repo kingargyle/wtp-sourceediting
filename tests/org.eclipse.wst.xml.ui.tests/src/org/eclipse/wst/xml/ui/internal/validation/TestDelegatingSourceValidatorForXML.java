284cc9cf18465259e7a2c8f2dfb2876773b8b0c3
nsandonato <nsandonato>
2009-08-10 15:40:08 +0000

    [285285] Removing comment end tag and adding it back leaves validation errors

--- tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/internal/validation/TestDelegatingSourceValidatorForXML.java
+++ tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/internal/validation/TestDelegatingSourceValidatorForXML.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,11 +23,15 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.wst.sse.core.StructuredModelManager;
+import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
+import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
 import org.eclipse.wst.validation.internal.core.ValidationException;
 import org.eclipse.wst.validation.internal.operations.WorkbenchContext;
 import org.eclipse.wst.validation.internal.provisional.core.IMessage;
 import org.eclipse.wst.validation.internal.provisional.core.IReporter;
 import org.eclipse.wst.validation.internal.provisional.core.IValidator;
+import org.eclipse.wst.xml.ui.tests.ProjectUtil;
 import org.eclipse.wst.xml.ui.tests.XMLUITestsPlugin;
 
 /**
@@ -108,53 +112,95 @@ public class TestDelegatingSourceValidatorForXML extends TestCase
 		assertFalse("Messages were reported on valid file 2.", reporter2.isMessageReported());
 	}
 	
-	private class TestReporter implements IReporter
-	{
-		protected boolean messageReported = false;
+	/**
+	 * Regression test for Bug 285285
+	 * 
+	 * @see org.eclipse.wst.xml.ui.internal.validation.TestDelegatingSourceValidatorForXML#testRemoveAndAddBackCommentEndTag
+	 * @see org.eclipse.wst.html.ui.tests.validation.TestHTMLValidator#testRemoveAndAddBackCommentEndTag
+	 * @see org.eclipse.jst.jsp.ui.tests.validation.JSPHTMLValidatorTest#testRemoveAndAddBackCommentEndTag
+	 */
+	public void testRemoveAndAddBackCommentEndTag() throws Exception{
+		String projectName = "RemoveAndAddBackCommentEndTag";
+		IProject project = ProjectUtil.createProject(projectName, null, null);
 		
-		public TestReporter(){
+		IFile testFile = null;
+		IStructuredModel model = null;
+		
+		try {
+			//get test file
+			ProjectUtil.copyBundleEntriesIntoWorkspace("testresources/RemoveAndAddBackCommentEndTag", projectName);
+			testFile = project.getFile("Test1.xml");
+			assertTrue("Test file " + testFile + " does not exist", testFile.exists());
 			
+			//get the document
+			model = StructuredModelManager.getModelManager().getModelForEdit(testFile);
+			IStructuredDocument document = model.getStructuredDocument();
+			
+			//set up for validator
+			WorkbenchContext context = new WorkbenchContext();
+			List fileList = new ArrayList();
+			fileList.add(testFile.getFullPath().toPortableString());
+			context.setValidationFileURIs(fileList);
+			
+			//validate clean file
+			TestReporter reporter = new TestReporter();
+			sourceValidator.validate(context, reporter);
+			assertFalse("There should be no validation errors on " + testFile, reporter.isMessageReported());
+			
+			//remove -->
+			document.replace(176, 3, "");
+			
+			//validate file with error
+			reporter = new TestReporter();
+			sourceValidator.validate(context, reporter);
+			assertTrue("There should be validation errors on " + testFile, reporter.isMessageReported());
+		
+			//replace -->
+			document.replace(176, 0, "-->");
+			
+			//validate clean file
+			reporter = new TestReporter();
+			sourceValidator.validate(context, reporter);
+			assertFalse("There should be no validation errors on " + testFile, reporter.isMessageReported());
+		} catch(ValidationException e) {
+			fail("Could not validate test file " + testFile + ": " + e.getMessage());
+		} finally {
+			if(model != null) {
+				model.releaseFromEdit();
+			}
 		}
+	}
+	
+	/**
+	 * A <code>IReporter</code> for testing validators
+	 */
+	private class TestReporter implements IReporter {
+		private boolean messageReported = false;
+		
+		public TestReporter(){}
 		
 		public void addMessage(IValidator origin, IMessage message) {
 			messageReported = true;
-			
 		}
 		
-		public boolean isMessageReported()
-		{
+		public boolean isMessageReported() {
 			return messageReported;
 		}
 
-		public void displaySubtask(IValidator validator, IMessage message) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void displaySubtask(IValidator validator, IMessage message) {}
 
 		public List getMessages() {
-			// TODO Auto-generated method stub
 			return null;
 		}
 
 		public boolean isCancelled() {
-			// TODO Auto-generated method stub
 			return false;
 		}
 
-		public void removeAllMessages(IValidator origin, Object object) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void removeAllMessages(IValidator origin, Object object) {}
 
-		public void removeAllMessages(IValidator origin) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void removeAllMessages(IValidator origin) {}
 
-		public void removeMessageSubset(IValidator validator, Object obj, String groupName) {
-			// TODO Auto-generated method stub
-			
-		}
-		
+		public void removeMessageSubset(IValidator validator, Object obj, String groupName) {}
 	}
 }
